278765d691455a82a6ca3475a8ce030747459da7
rosstimo <rosstimo@isu.edu>
2025-09-17 13:20:15 -0600

    example project

--- Example/Example.X/main.s
+++ Example/Example.X/main.s
@@ -1,87 +1,139 @@
-;Tim Rossiter
-;Fall 2025
-
-;ReferencesMPLAB® XC8 PIC® Assembler User's Guide
-;https://ww1.microchip.com/downloads/en/DeviceDoc/MPLAB%20XC8%20PIC%20Assembler%20User's%20Guide%2050002974A.pdf
-;
-;MPLAB® XC8 PIC Assembler User's Guide for Embedded Engineers
-;https://www.google.com/search?q=pic+assembly+user%27s+guide
-
-PROCESSOR 16F883 ; this is only if code is written specifically for this device
+; Tim Rossiter
+; Fall 2025
     
-RADIX dec ;this sets the default number base for constant/literal values. Can be hex, dec, oct. Should be dec if ommited
+; References: 
+/*
+MPLAB® XC8 PIC® Assembler User's Guide (DS50002974A)
+https://ww1.microchip.com/downloads/en/DeviceDoc/MPLAB%20XC8%20PIC%20Assembler%20User's%20Guide%2050002974A.pdf
+     
+MPLAB® XC8 PIC Assembler User's Guide for Embedded Engineers
 
-; PIC16F883 Configuration Bit Settings
-; Assembly source line config statements
+*/
+PROCESSOR 16F883             ; Only if code is written specifically for this device
+
+RADIX dec                    ; Set default number base for constant/literal values
+
+; --- PIC16F883 Configuration Bit Settings ---
 ; CONFIG1
-  CONFIG  FOSC = XT             ; Oscillator Selection bits (XT oscillator: Crystal/resonator on RA6/OSC2/CLKOUT and RA7/OSC1/CLKIN)
-  CONFIG  WDTE = OFF            ; Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
-  CONFIG  PWRTE = OFF           ; Power-up Timer Enable bit (PWRT disabled)
-  CONFIG  MCLRE = ON            ; RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
-  CONFIG  CP = OFF              ; Code Protection bit (Program memory code protection is disabled)
-  CONFIG  CPD = OFF             ; Data Code Protection bit (Data memory code protection is disabled)
-  CONFIG  BOREN = OFF           ; Brown Out Reset Selection bits (BOR disabled)
-  CONFIG  IESO = OFF            ; Internal External Switchover bit (Internal/External Switchover mode is disabled)
-  CONFIG  FCMEN = OFF           ; Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
-  CONFIG  LVP = OFF             ; Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
-
+CONFIG  FOSC = XT            ; XT oscillator: Crystal/resonator on RA6/OSC2 and RA7/OSC1
+CONFIG  WDTE = OFF           ; Watchdog Timer Enable bit (WDT disabled)
+CONFIG  PWRTE = OFF          ; Power-up Timer Enable bit (disabled)
+CONFIG  MCLRE = ON           ; MCLR pin function is MCLR
+CONFIG  CP = OFF             ; Program memory code protection is disabled
+CONFIG  CPD = OFF            ; Data memory code protection is disabled
+CONFIG  BOREN = OFF          ; Brown out Reset disabled
+CONFIG  IESO = OFF           ; Internal/External Switchover disabled
+CONFIG  FCMEN = OFF          ; Fail-Safe Clock Monitor disabled
+CONFIG  LVP = OFF            ; Low Voltage Programming OFF (HV on MCLR for programming)
 ; CONFIG2
-  CONFIG  BOR4V = BOR40V        ; Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
-  CONFIG  WRT = OFF             ; Flash Program Memory Self Write Enable bits (Write protection off)
+CONFIG  BOR4V = BOR40V
+CONFIG  WRT = OFF
 
-// config statements should precede project file includes.
-
-;Include Statements
-;These contain all the label/keyword definitions
-;You may want to find these and have a look inside
+; --- Include Device/Tool Definitions ---
 #include <xc.inc>
 #include <pic16f883.inc>
 
-;Code Section
-;-----------------------------------------------------
-    
-;Register/Variable Setup
-    SOMEVALUE EQU 0x20 ; assign a label to a register address
-    
-;Start of Program
+; --- Variable Definitions ---
+SOMEVALUE      EQU 0x20            ; user RAM variable
+WTEMP          EQU 0x70            ; WREG save (common RAM)
+STATUSTEMP     EQU 0x71            ; STATUS save (common RAM)
+     
+     
+     
+;Program Sections (PSECT)
 
-;Reset vector
+/* The following PSECTs are already defined in pic16f883.inc
+     
+//Memory Spaces (see Assembler User's Guide Table: 4-11 for for space flags)
+#define SPACE_CODE   0
+#define SPACE_DATA   1
+#define SPACE_EEPROM 3
+
+psect udata_shr,class=COMMON,space=SPACE_DATA,noexec
+psect udata,class=RAM,space=SPACE_DATA,noexec
+psect udata_bank0,class=BANK0,space=SPACE_DATA,noexec
+psect udata_bank1,class=BANK1,space=SPACE_DATA,noexec
+psect udata_bank2,class=BANK2,space=SPACE_DATA,noexec
+psect code,class=CODE,space=SPACE_CODE,delta=2
+psect data,class=STRCODE,space=SPACE_CODE,delta=2,noexec
+psect edata,class=EEDATA,space=SPACE_EEPROM,delta=2,noexec
+*/
+     
+;If you want to tell the linker to put the code in a specific program memory address
+;terminal option: -Wl,-presetVec=0h,-Wl,-pisrVec=4h
+;MPLABX:
+;Project Properties ? pic-as Linker ? General ? ?Custom linker options?:
+;Add: -presetVec=0h
+;Add: -pisrVec=4h
+;and so on...
+     
+     
+; --- Reset and Interrupt Vectors ---
 PSECT resetVect,class=CODE,delta=2
     GOTO Start
 
+PSECT isrVect,class=CODE,delta=2                   
+    GOTO ISR_Handler
 
-       
-;Setup Code that runs once at power up/reset
+; --- Main Code Section ---
 PSECT code,class=CODE,delta=2
+
 Start:
     BANKSEL INTCON
-    CLRF INTCON
-    
-    BANKSEL OPTION_REG
-    CLRF OPTION_REG
-    
-    BANKSEL ANSELH
-    CLRF ANSELH
-    
-    BANKSEL IOCB
-    CLRF IOCB
-    
-    BANKSEL PORTB
-    CLRF PORTB
-    
-    BANKSEL TRISB
-    CLRF TRISB
-    
-    BANKSEL WPUB
-    MOVLW 0xFF
-    MOVWF WPUB
-    
-    GOTO MainLoop
-    
-;Main Program Loop (Loops forever)
-MainLoop:
-    INCF PORTB,1
-    GOTO MainLoop
-    
-END ; End of code. This is required
+    CLRF   INTCON
 
+    BANKSEL OPTION_REG
+    CLRF   OPTION_REG
+
+    BANKSEL ANSELH
+    CLRF   ANSELH
+
+    BANKSEL IOCB
+    CLRF   IOCB
+
+    BANKSEL PORTB
+    CLRF   PORTB
+
+    BANKSEL TRISB
+    CLRF   TRISB
+
+    BANKSEL WPUB
+    MOVLW  0xFF
+    MOVWF  WPUB
+
+    ; ----- Interrupt Enable Example -----
+    BANKSEL INTCON
+    BSF    INTCON,6       ; Peripheral Interrupt Enable
+    BSF    INTCON,7       ; Global Interrupt Enable (Do this last)
+    GOTO   MainLoop
+
+; --- Interrupt Service Routine: Save/Restore Context (W,STATUS) ---
+PSECT code
+ISR_Handler:
+    MOVWF  WTEMP              ; Save WREG (common RAM, so no banking required)
+    SWAPF  STATUS,W           ; Save STATUS to WREG (swap to avoid affecting flags)
+    MOVWF  STATUSTEMP         ; Save STATUS
+
+    ; (Add interrupt source checks and logic here...)
+    ; Example: check TMR0IF and handle; clear flags as needed
+
+    ; --- End of ISR, restore context ---
+    SWAPF  STATUSTEMP,W       ; Restore STATUS to proper value
+    MOVWF  STATUS
+    SWAPF  WTEMP,F
+    SWAPF  WTEMP,W
+
+    RETFIE
+
+; --- Lookup Table Section (example: sine wave values as bytes) ---
+PSECT tblCode,class=CODE,delta=1
+LookupTbl:
+    DB 0,25,49,71,90,107,120,127,127,120,107,90,71,49,25,0
+
+PSECT code
+; --- Main Program Loop (Loops Forever) ---
+MainLoop:
+    INCF   PORTB,1
+    GOTO   MainLoop
+
+END     ; Required by assembler
--- Example/Example.X/nbproject/Makefile-local-default.mk
+++ Example/Example.X/nbproject/Makefile-local-default.mk
@@ -23,14 +23,14 @@ OS_CURRENT="$(shell uname -s)"
 # MP_CC is not defined
 # MP_CPPC is not defined
 # MP_BC is not defined
-MP_AS="/opt/microchip/xc8/v3.00/pic-as/bin/pic-as"
-MP_LD="/opt/microchip/xc8/v3.00/pic-as/bin/pic-as"
+MP_AS="/opt/microchip/xc8/v3.10/pic-as/bin/pic-as"
+MP_LD="/opt/microchip/xc8/v3.10/pic-as/bin/pic-as"
 # MP_AR is not defined
 DEP_GEN=${MP_JAVA_PATH}java -jar "/opt/microchip/mplabx/v6.20/mplab_platform/platform/../mplab_ide/modules/../../bin/extractobjectdependencies.jar"
 # MP_CC_DIR is not defined
 # MP_CPPC_DIR is not defined
 # MP_BC_DIR is not defined
-MP_AS_DIR="/opt/microchip/xc8/v3.00/pic-as/bin"
-MP_LD_DIR="/opt/microchip/xc8/v3.00/pic-as/bin"
+MP_AS_DIR="/opt/microchip/xc8/v3.10/pic-as/bin"
+MP_LD_DIR="/opt/microchip/xc8/v3.10/pic-as/bin"
 # MP_AR_DIR is not defined
 DFP_DIR=/opt/microchip/mplabx/v6.20/packs/Microchip/PIC16Fxxx_DFP/1.6.156
