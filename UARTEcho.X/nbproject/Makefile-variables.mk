4e415320f897848626803a8981875a58fcf4e713
Tim Rossiter <rosstimo@isu.edu>
2026-03-16 14:31:36 -0600

    working UART echo

---
+++ UARTEcho.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=UARTEcho.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/UARTEcho.X.production.hex
